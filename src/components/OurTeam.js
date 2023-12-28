21ebb86bed2ba3c2b20d68d7886cf496dfb92498
HuSTLeR1709 <nabhjeetkatiyar3@gmail.com>
2023-12-28 20:55:13 +0530

    some changes in footer

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -1,3 +1,5 @@
+import './ourteam.css'
+
 const people = [
     {
       name: 'Nabhjeet Katiyar',
@@ -35,15 +37,15 @@ const people = [
   export default function OurTeam() {
     return (
       <div className="bg-white py-18 sm:py-32">
-        <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-6 xl:grid-cols-3">
-          <div className="max-w-2xl">
-            <h2 className="text-3xl font-bold tracking-tight text-orange-600 sm:text-4xl">Meet our leadership</h2>
+        <div className="holder mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-6 xl:grid-cols-3">
+          <div className="texthold max-w-2xl">
+            <h2 className="teamhead text-3xl font-bold tracking-tight text-orange-600 sm:text-4xl">Meet our leadership</h2>
             <p className="mt-6 text-lg leading-8 text-gray-600">
             At the helm of SwiftCart, our leadership team embodies a passion for innovation, a commitment to excellence, and a vision for transforming your shopping experience.
             </p>
             
           </div>
-          <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+          <ul role="list" className="teamlist grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-8 xl:col-span-2">
             {people.map((person) => (
               <li key={person.name}>
                 <div className="flex items-center gap-x-6">
